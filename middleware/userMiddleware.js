86e438c70c517d5c5917d44270063e309e1cbca7
Matthew Renze <merenze@gmail.com>
2023-08-25 19:23:24 -0500

    Remove passwordHash from user endpoint result

--- middleware/userMiddleware.js
+++ middleware/userMiddleware.js
@@ -2,14 +2,20 @@
 const { User } = require("../models/");
 
 module.exports = {
-  userExists: async (req, res, next) =>
+  // Get the user from the route param and attach it to the request.
+  findUser: async (req, res, next) =>
     User.findByPk(req.params.userId).then((user) => {
       if (user) {
-        console.debug("User found.");
         req.user = user;
-          next();
-          return;
+        next();
+        return;
       }
       res.status(404).send();
     }),
+  
+  // Remove sensitive data.
+  sanitizeUser: (req, res, next) => {
+    req.user.passwordHash = undefined;
+    next();
+  },
 };
