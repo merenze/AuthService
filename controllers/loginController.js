ad9ce3fbc96a20370946760cc6ac1bfa3f8ccd41
Matthew Renze <merenze@gmail.com>
2023-08-25 22:32:57 -0500

    Client session ID stored in Client-Session header on successful login

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,80 +1,5 @@
 // controllers/login.js
-const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const { User } = require("../models/");
-
-/**
- * Compares the password against the one stored for the user.
- * @param {User} user
- * @param {string} password
- * @returns The passed user
- * @throws Response as { status, body } if the comparison fails.
- */
-const comparePassword = async (user, password) => {
-  const result = await bcrypt.compare(password, user.passwordHash);
-  if (result) {
-    return user;
-  }
-  throw {
-    status: 404,
-    body: { message: "Invalid email or password." },
-  };
-};
-
-/**
- * Verifies that the user's email has been validated.
- * @param {User} user
- * @returns The passed user
- * @throws Response as { status, body } if the email is not validated.
- */
-const emailValidated = (user) => {
-  if (!user.emailValidatedAt) {
-    console.debug("Not validated");
-    throw {
-      status: 403,
-      body: { message: "Email address not yet validated." },
-    };
-  }
-  return user;
-};
-
-/**
- * Stores the session ID on the user.
- * @param {User} user
- */
-const assignSession = (res, user) =>
-  res.cookie(
-    "session",
-    jwt.sign(
-      {
-        sub: user.id,
-      },
-      process.env.JWT_KEY
-    )
-  );
-
-/**
- * Handles a client error.
- * @param {*} res
- * @param {number} status
- * @param {*} body
- */
-const handleClientError = (res, status, body) => {
-  console.debug(status);
-  res.status(status).json(body);
-};
-/**
- * Handles an internal error.
- * @param {*} res
- * @param {*} error
- */
-const handleServerError = (res, error) => {
-  console.error("Login failed:", error);
-  res.status(500).json({
-    message:
-      process.env.NODE_ENV === "development" ? error.message : "Internal error",
-  });
-};
 
 /**
  * The login controller action.
@@ -82,14 +7,18 @@ const handleServerError = (res, error) => {
  * @param {*} res
  * @param {*} next
  */
-module.exports = async (req, res, next) =>
-  User.findOne({
-    where: {
-      email: req.body.email,
-    },
-  })
-    .then((user) => comparePassword(user, req.body.password))
-    .then((user) => emailValidated(user))
-    .then((user) => assignSession(res, user))
-    .catch(({ status, body }) => handleClientError(res, status, body))
-    .catch((error) => handleServerError(res, error));
+module.exports = (req, res) =>
+  res
+    .status(200)
+    .set(
+      "Client-Session",
+      jwt.sign(
+        {
+          sub: req.user.id,
+          purpose: "client-session",
+          // TODO: EV timeout period
+        },
+        process.env.JWT_KEY
+      )
+    )
+    .send();
