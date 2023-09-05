6a4966932265bea8b7bbd7ce821eb1a90c8a8a9b
Matthew Renze <merenze@gmail.com>
2023-09-05 15:27:54 -0500

    Write register tests

--- tests/register.test.js
+++ tests/register.test.js
@@ -1,27 +1,56 @@
 // tests/auth.test.js
-const { describe } = require("mocha");
+const { describe, done } = require("mocha");
 const app = require("../app");
 const supertest = require("supertest");
 const request = supertest(app);
+const assert = require("assert");
 
-describe("Register endpoint", () => {
-  it("should respond with status 400 when email is not present", () => {
-    // TODO
+describe("Register endpoint", async () => {
+  it("should respond with status 400 when email is not present", (done) => {
+    request
+      .post("/register")
+      .send({ password: "johnsmithpw"})
+      .expect(400)
+      .end((err, res) => err ? done(err) : done());
   });
 
-  it("should respond 400 when password is not present", () => {
-    // TODO
+  it("should respond 400 when password is not present", (done) => {
+    request
+      .post("/register")
+      .send({ email: "john.smith@example.com"})
+      .expect(400)
+      .end((err, res) => (err ? done(err) : done()));
   });
 
-  it("should respond 400 when email and password are not present", () => {
-    // TODO
+  it("should respond 400 when email and password are not present", (done) => {
+    request
+      .post("/register")
+      .send({})
+      .expect(400)
+      .end((err, res) => (err ? done(err) : done()));
   });
 
-  it("should respond 201 when email and password are both present", () => {
-    // TODO
+  it("should respond 400 when email format is invalid", (done) => {
+    request
+      .post("/register")
+      .send({ email: "john.smith", password: "johnsmithpw" })
+      .expect(400)
+      .end((err, res) => (err ? done(err) : done()));
   });
 
-  it("should respond 200 when the email is already in the database", () => {
-    // TODO
+  it("should respond 201 when email and password are both present", (done) => {
+    request
+      .post("/register")
+      .send({ email: "john.smith@example.com", password: "johnsmithpw" })
+      .expect(201)
+      .end((err, res) => (err ? done(err) : done()));
+  });
+
+  it("should respond 400 when the email is already in the database", (done) => {
+    request
+      .post("/register")
+      .send({ email: "john.smith@example.com", password: "johnsmithpw" })
+      .expect(400)
+      .end((err, res) => (err ? done(err) : done()));
   });
 });
